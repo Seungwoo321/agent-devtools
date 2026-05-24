5f0dc97b26b5a0a2c9e8ac652f6644c3972347bb
Seungwoo321 <seungwoo321@gmail.com>
2026-05-24 20:23:58 +0900

    chore(sveltekit): add generated server build artifacts
    SvelteKit 예제의 서버 출력물과 adapter-node 산출물을 추가했습니다. manifest, route node, 페이지 엔트리, 서버 훅 번들을 포함해 SSR 실행에 필요한 생성 파일을 반영했습니다.

---
+++ examples/sveltekit/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,12 @@
+export const index = 1;
+let component_cache;
+export const component = async () =>
+  (component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default);
+export const imports = [
+  '_app/immutable/nodes/1.D__wEODW.js',
+  '_app/immutable/chunks/eYpR7Yn_.js',
+  '_app/immutable/chunks/Bb-pn1ur.js',
+  '_app/immutable/chunks/DEDqjojZ.js',
+];
+export const stylesheets = [];
+export const fonts = [];
